28d5419e12809dba789827202361928510a527eb
Vanessa Dumeaux <vanessadumeaux@gmail.com>
2017-06-26 13:55:01 -0400

    add precomputed fdr for asssoc between modules and clinical variables

--- R/mixt.R
+++ R/mixt.R
@@ -1,4 +1,4 @@
 .onLoad <- function(libname, pkgname) {
-  utils::data("bresat", "moduleColors", "goterms", "combat_data", "msigdb", "perm_cor_p", "TOM-net", package=pkgname, envir=parent.env(environment()))
+  utils::data("bresat", "moduleColors", "goterms", "combat_data", "msigdb", "perm_cor_p", "mod_clinical_fdr","TOM-net", package=pkgname, envir=parent.env(environment()))
   #datasets = c("merged_bresat.RData", "merged_moduleColors.RData", "topGO_merged_mod.RData", "go_common.RData", "msigdb.RData", "combat_data.RData", "merged_TOM.RData")
 }
