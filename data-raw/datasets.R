2ac12abb50d36afdac68cd897221db3891c28539
Bjørn Fjukstad <bjorn@cs.uit.no>
2017-07-10 09:41:22 -0400

    dataset naming update

--- data-raw/datasets.R
+++ data-raw/datasets.R
@@ -17,9 +17,6 @@ load(url(paste0(dir,"/data/dat.RData")))
 devtools::use_data(dat,  overwrite=TRUE, compress="gzip")
 
 load(url(paste0(dir,"/data/mod_clinical_fdr.RData")))
-# the mod_clinical_fdr file contains an object named fdr not mod_clinical_fdr
-# so we'll create a new object with the correct name
-mod_clinical_fdr = fdr
 devtools::use_data(mod_clinical_fdr,  overwrite=TRUE, compress="gzip")
 
 load(url(paste0(dir,"/data/perm_cor_p.RData")))
