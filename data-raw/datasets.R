750fd99e9221e2d7ddd5c8a1546645a16faf4705
Vanessa Dumeaux <vanessadumeaux@Vanessas-MacBook-Pro.local>
2024-10-05 12:53:16 -0400

    edit datasets

--- data-raw/datasets.R
+++ data-raw/datasets.R
@@ -1,31 +1,31 @@
 # Fetch Stroma Datasets
 
-dir = 'https://rawgit.com/vdumeaux/mixtData/master'
+dir = getwd()
 
 # Datasets we can include directly.
-load(url(paste0(dir,"/data/bresat.RData")))
-devtools::use_data(bresat,  overwrite=TRUE, compress="gzip")
+load(file.path(dir,"data/bresat.rda"))
+# devtools::use_data(bresat,  overwrite=TRUE, compress="gzip")
 
-load(url(paste0(dir,"/data/moduleColors.RData")))
-devtools::use_data(moduleColors,  overwrite=TRUE, compress="gzip")
+load(paste0(dir,"/data/moduleColors.rda"))
+# devtools::use_data(moduleColors,  overwrite=TRUE, compress="gzip")
 
-load(url(paste0(dir,"/data/msigdb_enrichment.RData")))
-msigdb.enrichment = msigdb_enrichment
-devtools::use_data(msigdb.enrichment,  overwrite=TRUE, compress="gzip")
+load(paste0(dir,"/data/msigdb.enrichment.rda"))
+# msigdb.enrichment = msigdb_enrichment
+# devtools::use_data(msigdb.enrichment,  overwrite=TRUE, compress="gzip")
 
-load(url(paste0(dir,"/data/dat.RData")))
-devtools::use_data(dat,  overwrite=TRUE, compress="gzip")
+load(paste0(dir,"/data/dat.rda"))
+# devtools::use_data(dat,  overwrite=TRUE, compress="gzip")
 
-load(url(paste0(dir,"/data/mod_clinical_fdr.RData")))
-devtools::use_data(mod_clinical_fdr,  overwrite=TRUE, compress="gzip")
+load(paste0(dir,"/data/mod_clinical_fdr.rda"))
+# devtools::use_data(mod_clinical_fdr,  overwrite=TRUE, compress="gzip")
 
-load(url(paste0(dir,"/data/perm_cor_p.RData")))
-devtools::use_data(perm_cor_p,  overwrite=TRUE, compress="gzip")
+load(paste0(dir,"/data/perm_cor_p.rda"))
+# devtools::use_data(perm_cor_p,  overwrite=TRUE, compress="gzip")
 
-load(url(paste0(dir,"/data/TOM_net.RData")))
-net = TOM_net
-devtools::use_data(net, overwrite=TRUE, compress="gzip")
+load(paste0(dir,"/data/net.rda"))
+# net = TOM_net
+# devtools::use_data(net, overwrite=TRUE, compress="gzip")
 
 # Datasets we need to wrangle a bit to include
-load(url(paste0(dir,"/data/goterms.RData")))
-devtools::use_data(goterms, overwrite=TRUE, compress="gzip")
+load(paste0(dir,"/data/goterms.rda"))
+# devtools::use_data(goterms, overwrite=TRUE, compress="gzip")
